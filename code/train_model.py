b0ae7db2fe9324e1819987e33787073a0e9f0c2f
Ling Fei Zhang <lzhang133@gmail.com>
2023-12-09 19:58:46 -0500

    build small paths list in train model

--- code/train_model.py
+++ code/train_model.py
@@ -1,5 +1,23 @@
-# 1. parse input as AST tree
+import pickle
+from icecream import ic
 
-# 2. pass data through model
 
-# 3. ???
+def get_data():
+    with open('code/test_data.pickle', 'rb') as file:
+        data = pickle.load(file)  # list of code (in dict)
+
+    return data
+
+
+def get_paths(data):
+    paths = []
+    for code in data:
+        path = code['paths']
+        paths.append(path)
+
+    return paths
+
+
+if __name__ == '__main__':
+    data = get_data()
+    paths = get_paths(data)
